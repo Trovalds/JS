68f5e45d73ad7d92e0afa7df37802e250f555520
Sérgio Gomes <trovalds@gmail.com>
2021-02-13 00:22:56 -0400

    Exibição Taxas de Juros - Formatação em Tabela

--- scripts.js
+++ scripts.js
@@ -29,22 +29,22 @@ function calculaValor() {
 
     $.getJSON('admincartoes.json', function (idAdmin) {
         CredAV = + (valor * idAdmin[admin].CredAV);
-        document.getElementById("CredAV").innerHTML = "Crédito à Vista: " + CredAV;
+        document.getElementById("CredAV").innerHTML = CredAV.toFixed(2);
         CredAV14d = + (valor * idAdmin[admin].CredAV14d);
-        document.getElementById("CredAV14d").innerHTML = "Crédito 14 Dias: " + CredAV14d;
+        document.getElementById("CredAV14d").innerHTML = CredAV14d.toFixed(2);
         CredAV30d = + (valor * idAdmin[admin].CredAV30d);
-        document.getElementById("CredAV30d").innerHTML = "Crédito 30 Dias: " + CredAV30d;
+        document.getElementById("CredAV30d").innerHTML = CredAV30d.toFixed(2);
         CredParAV = + (valor * idAdmin[admin].CredParAV);
-        document.getElementById("CredParAV").innerHTML = "Crédito Parcelado à Vista: " + CredParAV;
+        document.getElementById("CredParAV").innerHTML = CredParAV.toFixed(2);
         CredPar14d = + (valor * idAdmin[admin].CredPar14d);
-        document.getElementById("CredPar14d").innerHTML = "Crédito Parcelado 14 Dias: " + CredPar14d;
+        document.getElementById("CredPar14d").innerHTML = CredPar14d.toFixed(2);
         CredPar30d = + (valor * idAdmin[admin].CredPar30d);
-        document.getElementById("CredPar30d").innerHTML = "Crédito Parcelado 30 Dias: " + CredPar30d;
+        document.getElementById("CredPar30d").innerHTML = CredPar30d.toFixed(2);
         Debito = + (valor * idAdmin[admin].Debito);
-        document.getElementById("Debito").innerHTML = "Débito: " + Debito;
+        document.getElementById("Debito").innerHTML = Debito.toFixed(2);
         DebitoCT = + (valor * idAdmin[admin].DebitoCT);
-        document.getElementById("DebitoCT").innerHTML = "Débito Caixa Tem: " + DebitoCT;
+        document.getElementById("DebitoCT").innerHTML = DebitoCT.toFixed(2);
         Pix = + (valor * idAdmin[admin].Pix);
-        document.getElementById("Pix").innerHTML = "PIX: " + Pix;
+        document.getElementById("Pix").innerHTML = Pix.toFixed(2);
     })
 }
